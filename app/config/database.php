d82fd6b71c217c5deabf036c12e87c306789107a
alchapa3 <al_chapa3@yahoo.com>
2016-08-04 11:21:28 -0500

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '104.131.18.69',
-			'database'  => getenv('DB_NAME', 'forge'),//'htk',
-			'username'  => getenv('DB_USERNAME', 'forge'),//'root',
-			'password'  => getenv('DB_PASSWORD', 'DCwyOQ49IAVQBK1k9wiS'),//'',
+			'host'      => $_ENV['DB_HOST'],
+			'database'  => $_ENV['DB_NAME'],
+			'username'  => $_ENV['DB_USERNAME'],
+			'password'  => $_ENV['DB_PASSWORD'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
