69b3e1f1d726e82975d780fea6c391e5dd2c61e6
alchapa3 <al_chapa3@yahoo.com>
2016-08-12 21:05:39 -0500

    Add files via upload

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -6,10 +6,13 @@
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
+	<link href='https://fonts.googleapis.com/css?family=Cinzel' rel='stylesheet' type='text/css'>
+
 	@yield('style')
 	<style type="text/css">
    		body { background: darkslategrey !important; }
    		body{ color: white !important; }
+   		body{ font-family: 'Cinzel', serif; }
 	</style>
 </head>
 <body>
